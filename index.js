cc15e716e4f0a01e255d5f2ebe0ee7c5c7704051
rodrigo <rodrigocabral.122@gmail.com>
2020-03-05 22:55:47 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express")
+const app = express()
+const PORT = 8081
+const bodyParser = require("body-parser")
+const connection = require("./database/database")
+const perguntaModel = require("./database/Pergunta")
+
+connection.authenticate().then(() => {
+    console.log("Conectado ao banco de dados")
+}).catch((erro) => {
+    console.log(erro)
+})
+
+app.set("view engine", "ejs")
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+app.get("/perguntar", (req, res) => {
+    res.render("perguntar")
+})
+
+app.post("/salvarpergunta", (req, res) => {
+    var titulo = req.body.titulo
+    var descricao = req.body.descricao
+    res.send("Titulo: " + titulo + "<br>" + "Descrição: " + descricao)
+})
+
+
+
+app.listen(PORT, () => {
+    console.log("Servidor rodando")
+})
